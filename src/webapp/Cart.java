f25e1ed429688f53b74714fdbe75320be14ea4ae
sophiamsi <sophia.malett@gmail.com>
2022-12-22 16:04:43 -0600

    Added new deals

--- src/webapp/Cart.java
+++ src/webapp/Cart.java
@@ -4,5 +4,7 @@ public class Cart {
     public static void main(String[] args) {
         System.out.println("Shopping card");
         System.out.println("Total"+" Promo code");
+        System.out.println("Deals");
     }
+
 }
