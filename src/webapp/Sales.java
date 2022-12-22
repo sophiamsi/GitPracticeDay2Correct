0ff43cd315a9e6fc20ec29bd570a8b7195d4cd17
sophiamsi <sophia.malett@gmail.com>
2022-12-22 16:51:16 -0600

    Sales updated

---
+++ src/webapp/Sales.java
@@ -0,0 +1,12 @@
+package webapp;
+
+public class Sales {
+    //15%
+    //20%
+    //...
+
+    //>>//
+    public static void main(String[] args) {
+        System.out.println("more sales items ");
+    }
+}
